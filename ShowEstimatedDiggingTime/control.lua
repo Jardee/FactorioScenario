879d06b80aee3c1344c8109580953f732c006e48
Dominik <aeduel@gmail.com>
2021-12-13 13:29:05 +0100

    Update control.lua
    Registered new events when entity is created / destroyed
    
    Thanks to  mrudat to let me know !

--- ShowEstimatedDiggingTime/control.lua
+++ ShowEstimatedDiggingTime/control.lua
@@ -13,6 +13,7 @@ script.on_init(function()
 			for key,value in ipairs(sentity.surface.find_entities_filtered{area = {{posx - radius, posy - radius}, {posx + radius, posy + radius}}, type = "resource"}) do 
 				count = count + value.amount
 			end
+
 			table.insert(global.Drills, {entity = sentity, text = rendering.draw_text{text = '?', surface = sentity.surface, target = {sentity.position.x - 0.75, sentity.position.y}, color = {255, 255, 255}}, prevcount = count, prevtick = game.tick, shouldupdate = game.tick + math.random(55, 100)})
 		end
 	end
@@ -26,99 +27,106 @@ script.on_event(defines.events.on_tick, function()
 			  if entity.shouldupdate < game.tick then 
 					if (entity.entity ~= nil and entity.entity.valid) then
                         if (entity.entity.status == 1) then
-                        
-					
+							posx = entity.entity.position.x 
+							posy = entity.entity.position.y
+							radius = entity.entity.prototype.mining_drill_radius
+							count = 0
 
-						posx = entity.entity.position.x 
-						posy = entity.entity.position.y
-						radius = entity.entity.prototype.mining_drill_radius
-						count = 0
-						for key,value in ipairs(entity.entity.surface.find_entities_filtered{area = {{posx - radius, posy - radius}, {posx + radius, posy + radius}}, type = "resource"}) do 
-							count = count + value.amount
-						end
-						
-						if entity.prevcount ~= count then
-					
+							for key,value in ipairs(entity.entity.surface.find_entities_filtered{area = {{posx - radius, posy - radius}, {posx + radius, posy + radius}}, type = "resource"}) do 
+								count = count + value.amount
+							end
+
+							if entity.prevcount ~= count then
+
+								estimatedticktime =  count / ( entity.entity.prototype.mining_speed * (entity.entity.speed_bonus + 1))
+
+								hour = math.floor(estimatedticktime / 3600)
+
+								estimatedticktime = estimatedticktime - hour * 3600
+
+								min = math.floor(estimatedticktime / 60)
+                        	    mins = tostring(min)
+
+								if min < 10 then
+									mins = '0' .. mins
+								end
+
+								estimatedticktime = estimatedticktime - min * 60
+
+								sec = math.floor(estimatedticktime)
+                        	    secs = tostring(sec)
+
+								if sec < 10 then
+									secs = '0' .. secs
+								end
+
+								time = hour .. ':' .. mins .. ':' .. secs
 							
-
-							estimatedticktime =  count / ( entity.entity.prototype.mining_speed * (entity.entity.speed_bonus + 1))
-
-							hour = math.floor(estimatedticktime / 3600)
-
-							estimatedticktime = estimatedticktime - hour * 3600
-
-							min = math.floor(estimatedticktime / 60)
-                            mins = tostring(min)
-							if min < 10 then
-								mins = '0' .. mins
+                        	    rendering.set_color(entity.text, {255, 255, 255})
+								rendering.set_text(entity.text, time)
 							end
 
-
-							estimatedticktime = estimatedticktime - min * 60
-
-							sec = math.floor(estimatedticktime)
-                            secs = tostring(sec)
-							if sec < 10 then
-								secs = '0' .. secs
-							end
-
-
-
-							time = hour .. ':' .. mins .. ':' .. secs
-					
-                            rendering.set_color(entity.text, {255, 255, 255})
-							rendering.set_text(entity.text, time)
-						end
-
-						entity.prevcount = count
-						entity.prevtick = game.tick
-						entity.shouldupdate = game.tick + math.random(55, 100)
-                    else
-                        rendering.set_color(entity.text, {255, 0, 0})
-                        rendering.set_text(entity.text, 'STOP')
-                    end
+							entity.prevcount = count
+							entity.prevtick = game.tick
+							entity.shouldupdate = game.tick + math.random(55, 100)
+                		else
+                		    rendering.set_color(entity.text, {255, 0, 0})
+                		    rendering.set_text(entity.text, 'STOP')
+                		end
 					else
 						rendering.destroy(global.Drills[k].text)
 						table.remove(global.Drills, k)
-
 					end
-			  end
+				end
 			end
 		end
 	end
 end)
 
 
-script.on_event(defines.events.on_built_entity, function(event)
 
-	if (event.created_entity.type == 'mining-drill') then
-		posx = event.created_entity.position.x 
-		posy = event.created_entity.position.y
-		radius = event.created_entity.prototype.mining_drill_radius
-		
-		count = 0
-		for key,value in ipairs(event.created_entity.surface.find_entities_filtered{area = {{posx - radius, posy - radius}, {posx + radius, posy + radius}}, type = "resource"}) do 
-			count = count + value.amount
-		end
+--events when it results in new mining-drill
 
-		table.insert(global.Drills, {entity = event.created_entity, text = rendering.draw_text{text = '?', surface = event.created_entity.surface, target = {event.created_entity.position.x - 0.75, event.created_entity.position.y}, color = {255, 255, 255}}, prevcount = count, prevtick = game.tick, shouldupdate = game.tick + math.random(55, 100)})
+--on_robot_built_entity
+--on_built_entity
+
+function miningDrillNewEventHandler(event)
+	posx = event.created_entity.position.x 
+	posy = event.created_entity.position.y
+	radius = event.created_entity.prototype.mining_drill_radius
+	
+	count = 0
+	for key,value in ipairs(event.created_entity.surface.find_entities_filtered{area = {{posx - radius, posy - radius}, {posx + radius, posy + radius}}, type = "resource"}) do 
+		count = count + value.amount
 	end
-end)
+	table.insert(global.Drills, {entity = event.created_entity, text = rendering.draw_text{text = '?', surface = event.created_entity.surface, target = {event.created_entity.position.x - 0.75, event.created_entity.position.y}, color = {255, 255, 255}}, prevcount = count, prevtick = game.tick, shouldupdate = game.tick + math.random(55, 100)})
+end
+
+script.on_event(defines.events.on_built_entity, miningDrillNewEventHandler, {{filter = "type", type = "mining-drill"}} )
+script.on_event(defines.events.on_robot_built_entity, miningDrillNewEventHandler, {{filter = "type", type = "mining-drill"}} )
 
 
-script.on_event(defines.events.on_player_mined_entity, function(event)
-    if (event.entity.type == 'mining-drill') then
-        if global.Drills ~= nil then
-            if #global.Drills > 0 then
-                for k, entity in pairs(global.Drills) do
-                    if (entity.entity.position.x == event.entity.position.x) and (entity.entity.position.y == event.entity.position.y) then
-                        rendering.destroy(entity.text)
-                        table.remove(global.Drills, k)
-                        break
-                    end
-                    
+
+--events when it results in destroying mining-drill
+
+--on_entity_died
+--on_robot_mined_entity
+---on_player_mined_entity
+
+function miningDrillDestroyEventHandler(event)
+    if global.Drills ~= nil then
+        if #global.Drills > 0 then
+            for k, entity in pairs(global.Drills) do
+                if (entity.entity.position.x == event.entity.position.x) and (entity.entity.position.y == event.entity.position.y) then
+                    rendering.destroy(entity.text)
+                    table.remove(global.Drills, k)
+                    break
                 end
             end
         end
     end
-end)
+end
+
+script.on_event(defines.events.on_player_mined_entity, miningDrillDestroyEventHandler, {{filter = "type", type = "mining-drill"}})
+script.on_event(defines.events.on_robot_mined_entity, miningDrillDestroyEventHandler, {{filter = "type", type = "mining-drill"}})
+script.on_event(defines.events.on_entity_died, miningDrillDestroyEventHandler, {{filter = "type", type = "mining-drill"}})
